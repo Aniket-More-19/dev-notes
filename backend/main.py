e0cd0fcec733dacbc039591c0cb9093e7b0c72bd
Aniket More <aniket19m@gmail.com>
2026-06-15 12:45:00 +0530

    updated backend routes using Note and NoteRequest classes with pydantic

--- backend/main.py
+++ backend/main.py
@@ -1,60 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, HTTPException, status
+from note import Note, NoteRequest
 
 app = FastAPI()
 
-notes = [
-    {
-        "id": 1,
-        "note": "Write .Dockerfile for dev-notes backend",
-        "created_at": "2026-06-01T12:00:00Z",
-        "updated_at": "2026-06-01T12:00:00Z",
-    },
-    {
-        "id": 2,
-        "note": "Add CI workflow for dev-notes",
-        "created_at": "2026-06-01T12:00:00Z",
-        "updated_at": "2026-06-01T12:00:00Z",
-    },
-    {
-        "id": 3,
-        "note": "Check failing tests for note.py",
-        "created_at": "2026-06-01T12:00:00Z",
-        "updated_at": "2026-06-01T12:00:00Z",
-    },
+NOTES = [
+    Note(1, "Write .Dockerfile for dev-notes backend", "2026-06-01T12:00:00Z", "2026-06-01T12:00:00Z"),
+    Note(2, "Update CI workflow for dev-notes", "2026-06-01T12:00:00Z", "2026-06-01T12:00:00Z"),
+    Note(3, "Check failing tests for note.py", "2026-06-01T12:00:00Z", "2026-06-01T12:00:00Z"),
 ]
 
-@app.get("/")
+# get all notes
+@app.get("/notes/", status_code=status.HTTP_200_OK)
 async def getAllNotes():
-    return {"notes": notes}
+    return NOTES
 
-@app.get("/{id}")
-async def getNoteById(id: int):
-    for note in notes:
-        if note["id"] == id:
+# get a note by it's id
+@app.get("/notes/{note_id}", status_code=status.HTTP_200_OK)
+async def getNoteById(note_id: int = Path(gt=0)):
+    for note in NOTES:
+        if note["id"] == note_id:
             return note
-    return {"error": "Note not found"}
+    raise HTTPException(status_code=404, detail="Note not found")
 
-@app.post("/createNote")
-async def createNote(note: dict):
-    notes.append({
-        "id": len(notes) + 1,
-        "note": note["note"],
-        "created_at": note["created_at"],
-        "updated_at": note["updated_at"],
-    })
+# create a new note
+@app.post("/notes/createNote", status_code=status.HTTP_201_CREATED)
+async def createNote(note_request: NoteRequest):
+    new_note = Note(**note_request.model_dump())
+    NOTES.append(new_note)
+    return new_note
 
-@app.put("/updateNote")
-async def updateNote(id: int, updatedNote: dict):
-    for note in notes:
-        if note["id"] == id:
-            note["todo"]=updatedNote["todo"]
-            note["created_at"]=updatedNote["created_at"]
-            note["updated_at"]=updatedNote["updated_at"]
-            break
+# update a note
+@app.put("/notes/updateNote", status_code=status.HTTP_200_OK)
+async def updateNote(note_id: int, updatedNote: NoteRequest):
+    for i, note in enumerate(NOTES):
+        if note["id"] == note_id:
+            updated_note = Note(id=note_id, **updateNote.model_dump())
+            NOTES[i] = updated_note
+            return updated_note
+    raise HTTPException(status_code=404, detail="Product not found")
 
-@app.delete("/deleteNote/{id}")
+
+@app.delete("/notes/deleteNote/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def deleteNote(id: int):
-    for note in notes:
+    for i, note in NOTES:
         if note["id"] == id:
-            notes.pop(notes.index(note))
+            NOTES.pop(i)
             return {"message": "Note deleted successfully"}
+    raise HTTPException(status_code=404, detail="Product not found")
