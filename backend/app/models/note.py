e150ac4ed8076a73ba25e6734a1117106a12b5fd
Aniket More <aniket19m@gmail.com>
2026-06-29 16:27:11 +0530

    Tested all apis working along with db and also tested apis with wrong payloads

--- backend/app/models/note.py
+++ backend/app/models/note.py
@@ -1,6 +1,7 @@
 import uuid
 from app.database import Base
-from sqlalchemy import Column, String, TIMESTAMP, Uuid
+from sqlalchemy import text, Column, String, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 
 
@@ -8,7 +9,7 @@ class Note(Base):
     __tablename__ = "notes"
 
 
-    id = Column(Uuid, primary_key=True, nullable=False, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default=text('gen_random_uuid()'))
     note = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
     updated_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now())
